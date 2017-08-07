d3eb361cce700de47d5d381633e25192050acd1e
cmm21 <chris.m.milbourne@gmail.com>
2017-08-07 13:21:19 -0400

    remove gem build code from gemspec

---
+++ lib/simple_line_icons-rails.rb
@@ -0,0 +1,1 @@
+require 'simple_line_icons/rails'
--- lib/simple_line_icons/rails.rb
+++ lib/simple_line_icons/rails.rb
@@ -1,7 +1,7 @@
+require "simple_line_icons/rails/engine"
 require "simple_line_icons/rails/version"
 
 module SimpleLineIcons
   module Rails
-    # Your code goes here...
   end
 end
